fe0f67d12095757e79457d0276e6230036ff5e3b
chris-gibs <theguyfromdubai@hotmail.com>
2021-07-05 17:01:38 +1000

    Altered all pt values to px, looks much better

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -3,7 +3,7 @@ import {useHistory} from "react-router-dom"
 const BackButton = () => {
   let history = useHistory()
   return (
-    <button id='back' type="button" onClick={history.goBack}>{"<-"}</button>
+    <button id='back' type="button" onClick={history.goBack} />
   )
 }
 
